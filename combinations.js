1b624a021a5f187f8845499674e2b5e8aaf6e102
Masato Iwaura <miwaura95@gmail.com>
2024-12-10 19:12:54 +0100

    Added the implementation of the combinations function

--- combinations.js
+++ combinations.js
@@ -1,5 +1,29 @@
 function combinations(digits){
-    return []
+    if(digits.length === 0){
+        return [];
+    }
+    const wordsMap ={
+        "2":["a","b","c"],
+        "3":["d","e","f"],
+        "4":["g","h","i"],
+        "5":["j","k","l"],
+        "6":["m","n","o"],
+        "7":["p","q","r","s"],
+        "8":["t","u","v"],
+        "9":["w","x","y","z"],
+    };
+    result = [""]
+    for(digit of digits){
+        const words = wordsMap[digit];
+        const tmp = [];
+        for(const prefix of result){
+            for(const word of words){
+                tmp.push(prefix+word);
+            }
+        }
+        result = tmp;
+    }
+    return result;
 }
 
 
